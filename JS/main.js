a1c9b95432d0889d2a2d5c8c1d4ffd753e9eb413
Nero <161507518+nerolyl@users.noreply.github.com>
2024-08-12 10:19:16 +0300

    slider_desktop

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+//swiper
+var swiper = new Swiper(".special_anime", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
